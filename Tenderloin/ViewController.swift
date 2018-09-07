f71537c496e1a6294a2d1682a11dc0fb6ea3aa2a
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-09-07 20:03:08 +0700

    Initial Commit

---
+++ Tenderloin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tenderloin
+//
+//  Created by Zulwiyoza Putra on 07/09/18.
+//  Copyright © 2018 Wiyoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
