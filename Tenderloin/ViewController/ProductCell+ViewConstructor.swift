6ae13cf8e10fe66093bd3432452ad4114eb30cbf
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-09-08 23:35:18 +0700

    dynamic rounding cell

--- Tenderloin/ViewController/ProductCell+ViewConstructor.swift
+++ Tenderloin/ViewController/ProductCell+ViewConstructor.swift
@@ -69,7 +69,7 @@ extension ProductCell {
             layer.masksToBounds = false
             layer.shadowColor = UIColor.gray.cgColor
             layer.shadowOffset = CGSize(width: 0, height: 0)
-            layer.shadowRadius = 4.0
+            layer.shadowRadius = 2.0
             layer.shadowOpacity = 0.5
             layer.shadowPath = path!
         }
